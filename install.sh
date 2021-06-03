222bad80e2867287f2b2ea72b1f97a979ceb6391
zapplv14 <83168318+zapplv14@users.noreply.github.com>
2021-06-03 23:40:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "INSTALL ZAPPLYZER PLATFORM VERSION 14"
+export LC_ALL=C.UTF-8
 sudo apt install python3-minimal build-essential python3-setuptools
 sudo apt install supervisor
 sudo apt install nginx
